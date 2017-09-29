8af4ef8d7fae6f0edb65bf3f1cded19f291dd52c
André Michel Andy Pozos <andreandyp@outlook.com>
2017-09-28 20:43:15 -0500

    Ahora se renderiza HTML </3

---
+++ cambiarhtml.bat
@@ -0,0 +1,5 @@
+call npm install --save ejs
+call npm install -g pug-cli
+call pug --pretty views/bracelezza.pug
+echo Ya puedes usar HTML en vez de pug :D
+echo Sin embargo aun puedes usar pug si lo deseas
--- index.js
+++ index.js
@@ -13,12 +13,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'html');
+app.engine('html', require("ejs").renderFile);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-  res.render("bracelezza");
+  res.render("bracelezza.html");
 });
 
 app.listen(app.get('port'), function() {
